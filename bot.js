23df9cefa8c957af35c64b4bed1a88564a7a7501
daviddalton <david@chaddalton.com>
2020-01-04 00:48:59 -0500

    first commit

---
+++ bot.js
@@ -0,0 +1,59 @@
+var Discord = require('discord.io');
+var logger = require('winston');
+var auth = require('./auth.json');
+// Configure logger settings
+logger.remove(logger.transports.Console);
+logger.add(new logger.transports.Console, {
+    colorize: true
+});
+logger.level = 'debug';
+// Initialize Discord Bot
+var bot = new Discord.Client({
+   token: auth.token,
+   autorun: true
+});
+bot.on('ready', function (evt) {
+    logger.info('Connected');
+    logger.info('Logged in as: ');
+    logger.info(bot.username + ' - (' + bot.id + ')');
+});
+
+bot.on('message', function (user, userID, channelID, message, evt) {
+    // Our bot needs to know if it will execute a command
+    // It will listen for messages that will start with `!`
+    if (message.substring(0, 1) == '!') {
+        var args = message.substring(1).split(' ');
+        var cmd = args[0];
+		var username = args[1];
+       
+        args = args.splice(1);
+        switch(cmd) {
+            //Easter Eggs lul
+            case 'doc':
+                bot.sendMessage({
+                    to: channelID,
+                    message: 'Doc is throwing my game!'
+                });
+				break;
+			case 'lifetime':
+				if (username) {
+					bot.sendMessage({
+					to: channelID,
+					message: 'This will show your lifetime stats ' + userID + '!'
+					});
+				} else {
+					bot.sendMessage({
+					to: channelID,
+					message: 'Add your username to the command so we know who you are.'
+					});
+				}
+			case 'testing':
+				
+				
+				
+            break;
+            // Just add any case commands if you want to..
+         }
+     }
+});
+
